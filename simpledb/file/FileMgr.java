4948b9ea0b37703d6af37259be9dc8f18a428d24
Amr2812 <amr.osama.elmohamady@gmail.com>
2024-10-16 19:48:21 +0300

    feat: file manager block statistics

--- simpledb/file/FileMgr.java
+++ simpledb/file/FileMgr.java
@@ -4,88 +4,96 @@ import java.io.*;
 import java.util.*;
 
 public class FileMgr {
-   private File dbDirectory;
-   private int blocksize;
-   private boolean isNew;
-   private Map<String,RandomAccessFile> openFiles = new HashMap<>();
+    private File dbDirectory;
+    private int blocksize;
+    private boolean isNew;
+    private Map<String, RandomAccessFile> openFiles = new HashMap<>();
+    private int blksRead = 0;
+    private int blksWritten = 0;
 
-   public FileMgr(File dbDirectory, int blocksize) {
-      this.dbDirectory = dbDirectory;
-      this.blocksize = blocksize;
-      isNew = !dbDirectory.exists();
+    public static record FileMgrStatistics(int blksRead, int blksWritten) {
+    }
 
-      // create the directory if the database is new
-      if (isNew)
-         dbDirectory.mkdirs();
+    public FileMgr(File dbDirectory, int blocksize) {
+        this.dbDirectory = dbDirectory;
+        this.blocksize = blocksize;
+        isNew = !dbDirectory.exists();
 
-      // remove any leftover temporary tables
-      for (String filename : dbDirectory.list())
-         if (filename.startsWith("temp"))
-         		new File(dbDirectory, filename).delete();
-   }
+        // create the directory if the database is new
+        if (isNew)
+            dbDirectory.mkdirs();
 
-   public synchronized void read(BlockId blk, Page p) {
-      try {
-         RandomAccessFile f = getFile(blk.fileName());
-         f.seek(blk.number() * blocksize);
-         f.getChannel().read(p.contents());
-      }
-      catch (IOException e) {
-         throw new RuntimeException("cannot read block " + blk);
-      }
-   }
+        // remove any leftover temporary tables
+        for (String filename : dbDirectory.list())
+            if (filename.startsWith("temp"))
+                new File(dbDirectory, filename).delete();
+    }
 
-   public synchronized void write(BlockId blk, Page p) {
-      try {
-         RandomAccessFile f = getFile(blk.fileName());
-         f.seek(blk.number() * blocksize);
-         f.getChannel().write(p.contents());
-      }
-      catch (IOException e) {
-         throw new RuntimeException("cannot write block" + blk);
-      }
-   }
+    public synchronized void read(BlockId blk, Page p) {
+        try {
+            RandomAccessFile f = getFile(blk.fileName());
+            f.seek(blk.number() * blocksize);
+            f.getChannel().read(p.contents());
+            blksRead++;
+        } catch (IOException e) {
+            throw new RuntimeException("cannot read block " + blk);
+        }
+    }
 
-   public synchronized BlockId append(String filename) {
-      int newblknum = length(filename);
-      BlockId blk = new BlockId(filename, newblknum);
-      byte[] b = new byte[blocksize];
-      try {
-         RandomAccessFile f = getFile(blk.fileName());
-         f.seek(blk.number() * blocksize);
-         f.write(b);
-      }
-      catch (IOException e) {
-         throw new RuntimeException("cannot append block" + blk);
-      }
-      return blk;
-   }
+    public synchronized void write(BlockId blk, Page p) {
+        try {
+            RandomAccessFile f = getFile(blk.fileName());
+            f.seek(blk.number() * blocksize);
+            f.getChannel().write(p.contents());
+            blksWritten++;
+        } catch (IOException e) {
+            throw new RuntimeException("cannot write block" + blk);
+        }
+    }
 
-   public int length(String filename) {
-      try {
-         RandomAccessFile f = getFile(filename);
-         return (int)(f.length() / blocksize);
-      }
-      catch (IOException e) {
-         throw new RuntimeException("cannot access " + filename);
-      }
-   }
+    public synchronized BlockId append(String filename) {
+        int newblknum = length(filename);
+        BlockId blk = new BlockId(filename, newblknum);
+        byte[] b = new byte[blocksize];
+        try {
+            RandomAccessFile f = getFile(blk.fileName());
+            f.seek(blk.number() * blocksize);
+            f.write(b);
+            blksWritten++;
+        } catch (IOException e) {
+            throw new RuntimeException("cannot append block" + blk);
+        }
+        return blk;
+    }
 
-   public boolean isNew() {
-      return isNew;
-   }
-   
-   public int blockSize() {
-      return blocksize;
-   }
+    public int length(String filename) {
+        try {
+            RandomAccessFile f = getFile(filename);
+            return (int) (f.length() / blocksize);
+        } catch (IOException e) {
+            throw new RuntimeException("cannot access " + filename);
+        }
+    }
 
-   private RandomAccessFile getFile(String filename) throws IOException {
-      RandomAccessFile f = openFiles.get(filename);
-      if (f == null) {
-         File dbTable = new File(dbDirectory, filename);
-         f = new RandomAccessFile(dbTable, "rws");
-         openFiles.put(filename, f);
-      }
-      return f;
-   }
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public int blockSize() {
+        return blocksize;
+    }
+
+    public FileMgrStatistics getStatistics() {
+        return new FileMgrStatistics(blksRead, blksWritten);
+    }
+
+    private RandomAccessFile getFile(String filename) throws IOException {
+        RandomAccessFile f = openFiles.get(filename);
+        if (f == null) {
+            File dbTable = new File(dbDirectory, filename);
+            f = new RandomAccessFile(dbTable, "rws");
+            openFiles.put(filename, f);
+        }
+        return f;
+    }
 }
